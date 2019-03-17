149684cd3f7b88b80b8865bd27f2b57a214a7454
naoya taguchi <astt.hwhw@gmail.com>
2019-03-17 16:39:30 +0900

    updated cuda ver

--- gpu_setup.sh
+++ gpu_setup.sh
@@ -13,7 +13,8 @@ wget http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/c
 sudo dpkg -i cuda-repo-ubuntu1604_10.0.130-1_amd64.deb
 rm -f cuda-repo-ubuntu1604_10.0.130-1_amd64.deb
 sudo apt-get update
-sudo apt-get install cuda nvidia-cuda-toolkit
+# sudo apt-get install cuda nvidia-cuda-toolkit
+sudo apt-get install cuda-10.0 nvidia-cuda-toolkit
 echo '------ CUDA VERSION ------'
 nvcc -V
 echo ''
