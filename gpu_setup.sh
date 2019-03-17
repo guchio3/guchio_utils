66cd8d1154a0ba6989cc7822319cedf52ed95f8c
naoya.taguchi <astt.hwhw@gmail.com>
2019-03-17 10:44:39 +0000

    added rm in gpu_setup.py

--- gpu_setup.sh
+++ gpu_setup.sh
@@ -14,6 +14,7 @@ sudo dpkg -i cuda-repo-ubuntu1604_10.1.105-1_amd64.deb
 sudo apt-key adv --fetch-keys http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/7fa2af80.pub
 sudo apt-get update
 sudo apt-get install -y cuda cuda-drivers
+rm cuda-repo-ubuntu1604_10.1.105-1_amd64.deb
 
 
 # install cuda 9.2
