ef837a7e73c2c8bbe0cf0c368bf7231f51b1d12e
naoya taguchi <astt.hwhw@gmail.com>
2019-03-17 20:22:22 +0900

    updated gpu_setup.py

--- gpu_setup.sh
+++ gpu_setup.sh
@@ -9,24 +9,10 @@ echo ''
 
 
 # install cuda 10.1
+# refer to https://developer.nvidia.com/cuda-downloads?target_os=Linux&target_arch=x86_64&target_distro=Ubuntu&target_version=1604&target_type=debnetwork
 wget http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/cuda-repo-ubuntu1604_10.1.105-1_amd64.deb
 sudo dpkg -i cuda-repo-ubuntu1604_10.1.105-1_amd64.deb
 sudo apt-key adv --fetch-keys http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/7fa2af80.pub
 sudo apt-get update
 sudo apt-get install -y cuda cuda-drivers
 rm cuda-repo-ubuntu1604_10.1.105-1_amd64.deb
-
-
-# install cuda 9.2
-#wget http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/cuda-repo-ubuntu1604_10.0.130-1_amd64.deb
-#sudo dpkg -i cuda-repo-ubuntu1604_10.0.130-1_amd64.deb
-#rm -f cuda-repo-ubuntu1604_10.0.130-1_amd64.deb
-#sudo apt-get update
-# sudo apt-get install cuda nvidia-cuda-toolkit
-#echo '------ CUDA VERSION ------'
-#nvcc -V
-#echo ''
-
-
-# install nvidia docker
-# sudo apt-get install -y nvidia-docker2=2.0.3+docker18.09.2 nvidia-container-runtime=2.0.0+18.09.2
